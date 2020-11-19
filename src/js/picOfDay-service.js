7d965825b68135bc4f053c72c4aee78fd97bba05
Amanda Otero <mandystar018@gmail.com>
2020-11-19 12:26:48 -0800

    Add functionality to the button and shows the pictures of the day for Nasa and title and description
    Co-authored-by: Taylor Baker <yao_shmitz17@yahoo.com>

--- src/js/picOfDay-service.js
+++ src/js/picOfDay-service.js
@@ -1,62 +1,17 @@
-let request = new XMLHttpRequest();
-const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`;
-
-request.onreadystatechange = function() {
-  if (this.readyState === 4 && this.status === 200) {
-    const response = JSON.parse(this.responseText);
-    getElements(response);
+export default class PicOfDay {  
+  static getPic() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
-
-request.open("GET", url, true);
-request.send();
-
-
-//Example with promises
-// let promise = new Promise(function(resolve, reject) {
-//   let request = new XMLHttpRequest();
-//   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-//   request.onload = function() {
-//     if (this.status === 200) {
-//       resolve(request.response);
-//     } else {
-//       reject(request.response);
-//     }
-//   }
-//   request.open("GET", url, true);
-//   request.send();
-// });
-
-
-
-
-// export default class PicOfDay {
-//   static getPic() {
-//     return fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-//     .then(function(response) {
-//       console.log(response.json())
-//       if(!response.ok) {
-//         throw Error(response.statusText);
-//       }
-//       return response.json();
-//     })
-//     console.log(response.json())
-//     .catch(function(error) {
-//       return error;
-//     });
-//   }
-// }
-
-// export default class PicOfDay {
-//   static async getPic() {
-//     try {
-//       const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`);
-//       if(!response.ok && response.status != 200) {
-//         throw Error(response.statusText);
-//       }
-//       return response.json();
-//     } catch(error) {
-//       return console.error(error.message);
-//     }
-//   }
-// }
--- src/main.js
+++ src/main.js
@@ -2,16 +2,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// usually when is more things to export import {Triangle} from './triangle.js'
+import PicOfDay from './js/picOfDay-service.js'
 
-
-// promise.then(function(response) {
-//   const body = JSON.parse(response);
-//   $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-//   $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-//   $('.showErrors').text("");
-// }, function(error) {
-//   $('.showErrors').text(`There was an error processing your request: ${error}`);
-//   $('.showHumidity').text("");
-//   $('.showTemp').text("");
-// });
+$(document).ready(function() {
+  $('#picOfDay').click(function() {
+    let promise = PicOfDay.getPic();
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('#picture-of-day').append(`<p>${body.title}</p>`);
+      $('#picture-of-day').append(`<img src="${body.url}">`);
+      $('#picture-of-day').append(`<p>${body.explanation}</p>`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+});
